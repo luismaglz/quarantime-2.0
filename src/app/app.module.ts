235bd4500bca862e376d7b5b6cef9966eb3db1c3
luismaglz <luismaglz@live.com>
2020-08-19 15:56:08 -0600

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { OnsenModule } from 'ngx-onsenui';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { OnsenModule } from "ngx-onsenui";
+
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, OnsenModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, OnsenModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule {}
